6a513399b08bf88eefa1e96678461d6ac187bbd5
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-01 17:38:42 +0530

    Update and rename restart.sh to start.sh

--- start.sh
+++ start.sh
@@ -32,5 +32,29 @@ sudo docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/
 
 echo 'Peer2 Joined Succesfully..'
 
-echo 'All Done.. You are Good to go..Bye...'
-exit 1
+echo 'All Done.................'
+
+
+echo 'Installing Chaincode on Peer0'
+
+sudo docker exec -it cli peer chaincode install -n mycc -p github.com/chaincode -v v0  #installing chaincode on peer 0 via cli
+
+echo 'Installing on Peer1'
+
+sudo docker-compose -f deployment/docker-compose-cli1.yml up -d
+sleep 3
+sudo docker exec -it cli1 peer chaincode install -n mycc -p github.com/chaincode -v v0  #installing chaincode on peer 1 via cli
+
+echo 'Installing on Peer2'
+sudo docker-compose -f deployment/docker-compose-cli2.yml up -d
+sleep 3
+
+sudo docker exec -it cli2 peer chaincode install -n mycc -p github.com/chaincode -v v0  #installing chaincode on peer 2 via cli
+
+echo 'Chaincode Succesfully installed on All Peers..'
+
+echo 'Instantiating Chaincode On Channel..'
+
+sudo docker exec -it cli2 peer chaincode instantiate -o orderer0.example.com:7050 -C mychannel -n mycc github.com/chaincode -v v0 -c '{"Args": ["a", "100"]}'
+
+echo 'All Done... You are Good to go.. Bye..'
