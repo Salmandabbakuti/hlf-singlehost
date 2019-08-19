d10e2feef7db85fdc8dbc5bc881b23ab2f617643
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-19 12:31:04 +0530

    Update teardown.sh

--- teardown.sh
+++ teardown.sh
@@ -6,7 +6,10 @@ sudo docker-compose -f deployment/docker-compose-cli.yml down
 sudo docker-compose -f deployment/docker-compose-cli1.yml down
 sudo docker-compose -f deployment/docker-compose-cli2.yml down
 
-cd deployment
+CURRENT_DIR=$PWD
+cd crypto-config/peerOrganizations/org1.example.com/ca/
+PRIV_KEY=$(ls *_sk)
+cd "$CURRENT_DIR/deployment
 sed -i "s/${PRIV_KEY}/CA_PRIVATE_KEY/g" docker-compose-kafka.yml
 echo 'All done...'
 
