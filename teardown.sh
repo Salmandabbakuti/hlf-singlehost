354b7b5a7abf3820055b3a646b93576dfba77d9a
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-19 12:22:54 +0530

    Update teardown.sh

--- teardown.sh
+++ teardown.sh
@@ -6,6 +6,8 @@ sudo docker-compose -f deployment/docker-compose-cli.yml down
 sudo docker-compose -f deployment/docker-compose-cli1.yml down
 sudo docker-compose -f deployment/docker-compose-cli2.yml down
 
+cd deployment
+sed -i "s/${PRIV_KEY}/CA_PRIVATE_KEY/g" docker-compose-kafka.yml
 echo 'All done...'
 
 exit 1
