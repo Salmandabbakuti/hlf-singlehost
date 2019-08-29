8b5dd41bc7ec6df61d2bcf9c88ba9f71d5ca6870
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-29 17:20:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo 'Network Restarting....'
 
 sudo docker-compose -f deployment/docker-compose-kafka.yml up -d
-sudo docker-compose -f deployment/docker-compose-cli.yml up -d
+
 echo 'Network Booting up... Should takes 20 seconds'
 sleep 20
 
@@ -41,13 +41,9 @@ sudo docker exec -it cli peer chaincode install -n mycc -p github.com/chaincode 
 
 echo 'Installing on Peer1'
 
-sudo docker-compose -f deployment/docker-compose-cli1.yml up -d
-sleep 3
 sudo docker exec -it cli1 peer chaincode install -n mycc -p github.com/chaincode -v v0  #installing chaincode on peer 1 via cli
 
 echo 'Installing on Peer2'
-sudo docker-compose -f deployment/docker-compose-cli2.yml up -d
-sleep 3
 
 sudo docker exec -it cli2 peer chaincode install -n mycc -p github.com/chaincode -v v0  #installing chaincode on peer 2 via cli
 
@@ -55,6 +51,6 @@ echo 'Chaincode Succesfully installed on All Peers..'
 
 echo 'Instantiating Chaincode On Channel..'
 
-sudo docker exec -it cli2 peer chaincode instantiate -o orderer0.example.com:7050 -C mychannel -n mycc github.com/chaincode -v v0 -c '{"Args": ["a", "100"]}'
+sudo docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C mychannel -n mycc github.com/chaincode -v v0 -c '{"Args": ["a", "100"]}'
 
 echo 'All Done... You are Good to go.. Bye..'
