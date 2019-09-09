9102ed8028d46e6e6002a4909d76f3d051745106
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-09 11:34:16 +0530

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,11 +1,9 @@
 echo 'Shutting Network Down...'
 
 sudo docker-compose -f deployment/docker-compose-kafka.yml down
+sudo docker volume prune
+sudo docker network prune
 sleep 2
-sudo docker-compose -f deployment/docker-compose-cli.yml down
-sudo docker-compose -f deployment/docker-compose-cli1.yml down
-sudo docker-compose -f deployment/docker-compose-cli2.yml down
-
 echo 'All done...'
 
 exit 1
