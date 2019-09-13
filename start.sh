05465cddc885b7c2b83da35693a80ad2a8b13535
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-13 11:40:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,11 +9,11 @@ sleep 20
 
 echo 'Channel Creation Taking Place...'
 
-sudo docker exec peer0.org1.example.com peer channel create -o orderer0.example.com:7050 -c mychannel -f /var/hyperledger/configs/channel.tx  #peer0 channel creation
+sudo docker exec -it peer0.org1.example.com peer channel create -o orderer0.example.com:7050 -c mychannel -f /var/hyperledger/configs/channel.tx  #peer0 channel creation
 
 echo 'Channel Creation done..'
 
-sudo docker exec peer0.org1.example.com peer channel join -b mychannel.block    #peer0 joins channel
+sudo docker exec -it peer0.org1.example.com peer channel join -b mychannel.block    #peer0 joins channel
 
 echo 'Peer0 joinined Channel...'
 
@@ -26,9 +26,9 @@ docker cp mychannel.block peer2.org1.example.com:/opt/gopath/src/github.com/hype
 rm mychannel.block
 
 echo 'Peers Joining Takes place...'
-sudo docker exec peer1.org1.example.com peer channel join -b mychannel.block  #peer1 joins channel
+sudo docker exec -it peer1.org1.example.com peer channel join -b mychannel.block  #peer1 joins channel
 echo 'Peer1 Joined Channel Successfully...'
-sudo docker exec peer2.org1.example.com peer channel join -b mychannel.block   #peer2 joins channel
+sudo docker exec -it peer2.org1.example.com peer channel join -b mychannel.block   #peer2 joins channel
 
 echo 'Peer2 Joined Succesfully..'
 
