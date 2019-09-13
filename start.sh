0004d7a8c4adb4b412b783f6f1b08a9019b07071
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-13 11:05:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,26 +7,26 @@ sleep 20
 
 echo 'Channel Creation Taking Place...'
 
-sudo docker exec -it cli peer channel create -o orderer0.example.com:7050 -c mychannel -f /var/hyperledger/configs/channel.tx  #peer0 channel creation
+sudo docker exec  peer0.org1.example.com peer channel create -o orderer0.example.com:7050 -c mychannel -f /var/hyperledger/configs/channel.tx  #peer0 channel creation
 
 echo 'Channel Creation done..'
 
-sudo docker exec -it cli peer channel join -b mychannel.block    #peer0 joins channel
+sudo docker exec peer0.org1.example.com peer channel join -b mychannel.block    #peer0 joins channel
 
 echo 'Peer0 joinined Channel...'
 
 echo 'copying mychannel.block from peer0 to peer1 and peer2'
 # copy mychannel.block to peer1 and peer2
-docker cp cli:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block .
+docker cp peer0.org1.example.com:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block .
 
-docker cp mychannel.block cli1:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
-docker cp mychannel.block cli2:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
+docker cp mychannel.block peer1.org1.example.com:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
+docker cp mychannel.block peer2.org1.example.com:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
 rm mychannel.block
 
 echo 'Peers Joining Takes place...'
-sudo docker exec -it cli1 peer channel join -b mychannel.block  #peer1 joins channel
+sudo docker exec peer1.org1.example.com peer channel join -b mychannel.block  #peer1 joins channel
 echo 'Peer1 Joined Channel Successfully...'
-sudo docker exec -it cli2 peer channel join -b mychannel.block   #peer2 joins channel
+sudo docker exec peer2.org1.example.com peer channel join -b mychannel.block   #peer2 joins channel
 
 echo 'Peer2 Joined Succesfully..'
 
