2ec76f471668879938d83847cb345710e23eeea3
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-28 23:32:56 +0530

    Create installEvents.sh

---
+++ installEvents.sh
@@ -0,0 +1,16 @@
+echo 'Installing chaincode on Peer0'
+sudo docker exec -it cli peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com" -l "node"
+sleep 3
+echo 'Installing chaincode on Peer1'
+sudo docker exec -it cli1 peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com" -l "node"
+sleep 3
+echo 'Installing chaincode on Peer2'
+sudo docker exec -it cli2 peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com" -l "node"
+
+sleep 5
+echo 'Instantiating chaincode..'
+sudo docker exec -it cli peer chaincode instantiate -o orderer0.example.com:7050 -C mychannel -n mycc -l "node" -v 1.0 -c '{"Args":[]}'
+
+
+echo 'All done..'
+exit 1
