741458d91b616ad26730b0d3243897f3400617e4
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-30 07:29:40 +0530

    Production Ready deployments, connection profiles, client model  (#13)
    * Create connection-org1.json
    
    * Update connection-org1.json
    
    * Create installEvents.sh
    
    * Create logic.js
    
    * Create index.js
    
    * Create package.json
    
    * Update installEvents.sh
    
    * Create prereqs.sh
    
    * Create enrollAdmin.js
    
    * Create registerUser.js
    
    * Create package.json
    
    * Create server.js
    
    * Rename connection-org1.json to connection.json
    
    Renamed for the convenience..
    
    * Update registerUser.js
    
    Connection profile location changes
    
    * Update enrollAdmin.js
    
    Connection profile location changes
    
    * Update server.js
    
    🧐
    
    * adding pregenerated Certificates
    
    * Update installEvents.sh
    
    Chaincode paths change based on mount path in Containeers
    
    * Update enrollAdmin.js
    
    * Update enrollAdmin.js
    
    * adding CA config
    
    * Update docker-compose-kafka.yml
    
    CA key Modifications
    
    * Update docker-compose-kafka.yml
    
    CA identity things
    
    * Create APIqueries.md
    
    * Update docker-compose-kafka.yml
    
    Mac seirra path sharing restrictions to docker
    
    * Update docker-compose-base.yml
    
    Mac docker path issues

---
+++ chaincode/events/logic.js
@@ -0,0 +1,77 @@
+'use strict';
+const { Contract} = require('fabric-contract-api');
+
+class eventLogs extends Contract {
+
+   async logEvents(ctx,productId) {
+   
+    let logsAsBytes = await ctx.stub.getState(productId); 
+    if (!logsAsBytes || logsAsBytes.toString().length <= 0) {
+      return("Error:Logs With this ProductId does not exist..!");
+       }
+    
+    else{
+     let logs=JSON.parse(logsAsBytes.toString());
+     return JSON.stringify(logs);
+     }
+ }
+    
+    async submitProduct(ctx,productId,ownerName,productName) {
+   
+    let productAsBytes = await ctx.stub.getState(productId); 
+    if (!productAsBytes || productAsBytes.toString().length <= 0) {
+      let productData={
+           ProductId:productId,
+           ProductName:productName,
+           Owner:ownerName,
+           Logs:[],
+           CurrentStatus:'Submitted'
+           };
+      let timeStamp= await ctx.stub.getTxTimestamp();
+      const timestamp = new Date(timeStamp.getSeconds() * 1000).toISOString();
+
+      let logData={
+           Timestamp:timestamp,
+           Status:'Submitted'
+           };
+    
+     productData.Logs.push(logData);
+
+     await ctx.stub.putState(productId,Buffer.from(JSON.stringify(productData))); 
+     console.log('Product Submission event added To the ledger Succesfully..');
+     return('Product Submission event added To the ledger Succesfully..');
+     }
+     else {
+
+      return('Error:Product With this Id Already Exists.!');
+     }
+  }
+
+  async addEvent(ctx,productId, status){
+
+   let productAsBytes = await ctx.stub.getState(productId); 
+    if (!productAsBytes || productAsBytes.toString().length <= 0) {
+    return('Error:Product With This Id does not exist..')
+     }
+    else {
+    
+    let timeStamp= await ctx.stub.getTxTimestamp();
+    const timestamp = new Date(timeStamp.getSeconds() * 1000).toISOString();
+
+      let logData={
+           Timestamp:timestamp,
+           Status:status
+            };
+        let product = JSON.parse(productAsBytes);
+        product.Logs.push(logData);
+        product.CurrentStatus=status;
+
+       await ctx.stub.putState(productId,Buffer.from(JSON.stringify(product))); 
+       console.log('Product event added To the ledger Succesfully..');
+       return('Product event added To the ledger Succesfully..');
+       }
+
+    }
+}
+
+module.exports=eventLogs;
